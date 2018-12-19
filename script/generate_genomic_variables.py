fc933a6965c73b49c3f22ccc0dbaf2e7904a9d06
dinhe878 <derrick.he@gmail.com>
2018-12-19 11:27:23 +0100

    add a script folder for various scripts

---
+++ script/generate_genomic_variables.py
@@ -0,0 +1,20 @@
+#!/opt/local/bin/python2.7
+import sys
+from math import log
+import numpy as np
+infile = open(sys.argv[1], 'r')
+outfile = open(sys.argv[1]+".variables", "w")
+outfile.write("Num_bp RS_Max RS_Min RS_Var\n")
+for line in infile.readlines():
+	line = line.rstrip()
+	RS = line.split(" ")
+	if len(RS) == 1:
+		for i in range(4):
+			outfile.write("0 100000 0 0\n")
+	else:
+		RS = map(int, RS)
+		for i in range(4):
+			outfile.write(str(len(RS))+" "+str(max(RS))+" "+str(min(RS))+" "+str(np.var(RS))+"\n")
+		
+infile.close()
+outfile.close()
