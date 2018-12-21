2bd75f06283354143fa39d9cedb4d0fa36d24f4d
dinhe878 <derrick.he@gmail.com>
2018-12-21 12:18:21 +0100

    fix the Num_bp calculation

--- script/generate_genomic_variables.py
+++ script/generate_genomic_variables.py
@@ -9,12 +9,12 @@ for line in infile.readlines():
 	line = line.rstrip()
 	RS = line.split(" ")
 	if len(RS) == 1:
-		for i in range(4):
+		for i in range(2):
 			outfile.write("0 100000 0 0\n")
 	else:
 		RS = map(int, RS)
-		for i in range(4):
-			outfile.write(str(len(RS))+" "+str(max(RS))+" "+str(min(RS))+" "+str(np.var(RS))+"\n")
+		for i in range(2):
+			outfile.write(str(len(RS)-1)+" "+str(max(RS))+" "+str(min(RS))+" "+str(np.var(RS))+"\n")
 		
 infile.close()
 outfile.close()
